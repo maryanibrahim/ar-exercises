82a0e25c1bdf3575dafa85070bc18a044a06f43c
maryan ibrahim <maryanibrahim301@yahoo.com>
2023-12-14 06:25:12 +0000

    Validations for both models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,38 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+# Employee Model 
+class Employee < ActiveRecord::Base
+    belongs_to :store
+  
+    validates :first_name, :last_name, :store, presence: true
+    validates :hourly_rate, numericality: { only_integer: true, greater_than: 39, less_than: 201 }
+  end
+
+  # Store Model
+  class Store < ActiveRecord::Base
+    has_many :employees
+  
+    validates :name, length: { minimum: 3 }
+    validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+    validate :must_carry_apparel
+  
+    private
+  
+    def must_carry_apparel
+      unless mens_apparel || womens_apparel
+        errors.add(:base, "Store must carry at least one of men's or women's apparel")
+      end
+    end
+  end
+
+  # Test Validations
+  puts "Enter a store name:"
+store_name = gets.chomp
+
+new_store = Store.create(name: store_name, annual_revenue: -100) 
+
+unless new_store.valid?
+  puts "Errors: #{new_store.errors.full_messages.join(", ")}"
+end
+
