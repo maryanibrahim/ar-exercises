87f5289959bcbff8a1015d112fbfdc7d6e683c43
maryan ibrahim <maryanibrahim301@yahoo.com>
2023-12-14 05:40:58 +0000

    destroy the third store

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -5,4 +5,11 @@ require_relative './exercise_2'
 puts "Exercise 3"
 puts "----------"
 
-# Your code goes here ...
+# Load the third store from the database and assign it to @store3
+@store3 = Store.find(3)
+
+# Using ActiveRecord's destroy method, delete the store from the database
+@store3.destroy
+
+# Verify that the store has been deleted by outputting the count
+puts "The number of stores in the database: #{Store.count}."
