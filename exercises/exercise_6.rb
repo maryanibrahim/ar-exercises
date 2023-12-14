975c6a37c7dff845b192c9ffaa055590a2c462f2
maryan ibrahim <maryanibrahim301@yahoo.com>
2023-12-14 06:11:59 +0000

    use the employees table

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,18 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+class Store < ActiveRecord::Base
+    has_many :employees
+  end
+
+  class Employee < ActiveRecord::Base
+    belongs_to :store
+  end
+
+  # Add employees to @store1
+@store1.employees.create(first_name: "Mark", last_name: "M", hourly_rate: 20)
+@store1.employees.create(first_name: "Marly", last_name: "M", hourly_rate: 20)
+
+# Add employees to @store2
+@store2.employees.create(first_name: "Mina", last_name: "M", hourly_rate: 40)
+@store2.employees.create(first_name: "Maryan", last_name: "M", hourly_rate: 30)
