459c4f95e2e9363f17b8f6a9813bb9b4a5fa9606
maryan ibrahim <maryanibrahim301@yahoo.com>
2023-12-14 05:38:07 +0000

    Update the first store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,12 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+# Load the first store (with id = 1) from the database and assign it to @store1
+@store1 = Store.find(1)
+
+# Load the second store from the database and assign it to @store2
+@store2 = Store.find(2)
+
+# Update the first store (@store1) instance in the database
+@store1.name = "ze"
+@store1.save
